fdbe7fbed1ddd00c0cc6f0f6457ca3a68b59b9d8
Ejay02 <e.jae02@gmail.com>
2025-01-24 13:21:00 +0100

    Update videoModal component to use iframe for video playback

--- src/components/aboutComponents/videoModal.vue
+++ src/components/aboutComponents/videoModal.vue
@@ -6,10 +6,9 @@
   >
     <div class="bg-white p-4 rounded-lg max-w-4xl w-full">
       <div class="aspect-video">
-        
         <!-- -->
-        <!-- src="/campus_tour.mp4" -->
         <iframe
+          src="/campus_tour.mp4"
           frameborder="0"
           allowfullscreen
           class="w-full h-full"
