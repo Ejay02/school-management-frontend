5f4ad670c60a3a0a19a23c69c85839be7d3fb748
Ejay02 <e.jae02@gmail.com>
2025-01-24 13:07:29 +0100

    Fix import path for VideoModal component in about.vue

--- src/views/infoPages/about.vue
+++ src/views/infoPages/about.vue
@@ -147,7 +147,7 @@
 
 <script setup>
 import { ref } from "vue";
-import VideoModal from "@/components/aboutComponents/VideoModal.vue";
+import VideoModal from "../../components/aboutComponents/videoModal.vue";
 
 const videoModalVisible = ref(false);
 
