9aa2cb1ae4bdcba477ed5f95aecba7ee57b3397a
Ejay02 <e.jae02@gmail.com>
2025-01-27 08:48:05 +0100

    Add back arrow navigation to login and signup pages for improved user experience

--- src/views/auth/signup.vue
+++ src/views/auth/signup.vue
@@ -4,6 +4,27 @@
     <div
       class="left w-full lg:w-1/2 flex flex-col justify-center px-6 py-12 lg:px-8 bg-gray-50"
     >
+      <!-- back arrow -->
+      <div class="pl-4">
+        <router-link to="/" class="group">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="text-gray-600 h-6 w-6 cursor-pointer group-hover:animate-bounce"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+            />
+          </svg>
+        </router-link>
+      </div>
+
+      <!-- icon -->
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
         <router-link to="/">
           <svg
