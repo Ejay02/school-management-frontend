ca630b2c03a5dcc3ac7704c2676133ebb68caad8
Ejay02 <e.jae02@gmail.com>
2025-01-27 10:01:38 +0100

    Add AboutHeader component to improve the structure of the About page

---
+++ src/components/aboutComponents/aboutHeader.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="relative bg-gray-200 shadow-md">
+    <div class="container mx-auto py-6 flex justify-between items-center">
+      <router-link to="/" class="group">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="text-gray-600 h-6 w-6 cursor-pointer group-hover:animate-bounce"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
+          />
+        </svg>
+      </router-link>
+      <!-- <nav class="space-x-4">
+          <router-link
+            to="/admissions"
+            class="text-gray-600 hover:text-eduSky transition"
+          >
+            Admissions
+          </router-link>
+          <router-link
+            to="/academics"
+            class="text-gray-600 hover:text-eduSky transition"
+          >
+            Academics
+          </router-link>
+        </nav> -->
+    </div>
+  </header>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/views/infoPages/about.vue
+++ src/views/infoPages/about.vue
@@ -1,40 +1,7 @@
 <template>
   <div class="min-h-screen bg-gradient-to-br from-indigo-50 to-sky-100">
     <!-- Header Section -->
-    <header class="relative bg-gray-200 shadow-md">
-      <div class="container mx-auto py-6 flex justify-between items-center">
-        <router-link to="/" class="group">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="text-gray-600 h-6 w-6 cursor-pointer group-hover:animate-bounce"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18"
-            />
-          </svg>
-        </router-link>
-        <!-- <nav class="space-x-4">
-          <router-link
-            to="/admissions"
-            class="text-gray-600 hover:text-eduSky transition"
-          >
-            Admissions
-          </router-link>
-          <router-link
-            to="/academics"
-            class="text-gray-600 hover:text-eduSky transition"
-          >
-            Academics
-          </router-link>
-        </nav> -->
-      </div>
-    </header>
+    <AboutHeader />
 
     <!-- Hero Section -->
     <AboutHero />
@@ -48,6 +15,7 @@
 </template>
 
 <script setup>
+import AboutHeader from "../../components/aboutComponents/aboutHeader.vue";
 import AboutHero from "../../components/aboutComponents/aboutHero.vue";
 import AboutLeadership from "../../components/aboutComponents/aboutLeadership.vue";
 import AboutVideo from "../../components/aboutComponents/aboutVideo.vue";
