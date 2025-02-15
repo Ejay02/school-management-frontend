08225c004b9e3ce289e68d762402187962edbc9f
Ejay02 <e.jae02@gmail.com>
2025-02-15 11:39:59 +0100

    Enhance login and signup forms with password visibility toggle and UI improvements

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -27,7 +27,7 @@
 
       <!-- icon -->
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <router-link to="/">
+        <!-- <router-link to="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -42,7 +42,7 @@
               d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
             />
           </svg>
-        </router-link>
+        </router-link> -->
 
         <h2
           class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-800"
@@ -92,17 +92,27 @@
                 >
               </div>
             </div>
-            <div class="mt-2">
+            <div class="mt-2 relative">
               <input
                 v-model="password"
-                type="password"
+                :type="showPassword ? 'text' : 'password'"
                 name="password"
                 id="password"
                 autocomplete="current-password"
-                placeholder="mySuperSecretPassword"
+                placeholder="my-super-secret-password"
                 required
                 class="cursor-pointer block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-800 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-eduPurpleLight sm:text-sm/6"
               />
+              <button
+                type="button"
+                class="cursor-pointer absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                @click="showPassword = !showPassword"
+              >
+                <i
+                  class="fa-regular"
+                  :class="showPassword ? 'fa-eye-slash' : 'fa-eye'"
+                ></i>
+              </button>
             </div>
           </div>
 
@@ -152,6 +162,7 @@ const notificationStore = useNotificationStore();
 
 const username = ref("");
 const password = ref("");
+const showPassword = ref(false);
 
 const { mutate: loginMutate } = useMutation(loginMutation);
 
--- src/views/auth/signup.vue
+++ src/views/auth/signup.vue
@@ -5,7 +5,7 @@
       class="left w-full lg:w-1/2 flex flex-col justify-center px-6 py-12 lg:px-8 bg-gray-50"
     >
       <!-- back arrow -->
-      <div class="pl-4">
+      <div class="pl-4 mt-4">
         <router-link to="/" class="group">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -26,7 +26,7 @@
 
       <!-- icon -->
       <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-        <router-link to="/">
+        <!-- <router-link to="/">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -41,16 +41,16 @@
               d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5"
             />
           </svg>
-        </router-link>
+        </router-link> -->
 
         <h2
-          class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-800"
+          class="mb-10 text-center text-2xl/9 font-bold tracking-tight text-gray-800"
         >
-          Create your account
+          Create your account 🚀
         </h2>
       </div>
 
-      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div class="mt-0 sm:mx-auto sm:w-full sm:max-w-sm">
         <form class="space-y-6" action="#" method="POST">
           <div class="flex justify-between">
             <div class="">
@@ -166,7 +166,7 @@
           </div>
         </form>
 
-        <p class="mt-10 text-center text-sm/6 text-gray-500">
+        <p class="mt-5 text-center text-sm/6 text-gray-500">
           Already have an account?
           <router-link
             to="/login"
